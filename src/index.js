8105627d95099fed1e5412a98f4e2083ddefbd90
arghadeep05 <arghadeep05@gmail.com>
2023-10-27 22:16:24 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'node_modules/@mdi/font/css/materialdesignicons.min.css';
-import 'node_modules/materialize-css/dist/css/materialize.min.css';
-import 'node_modules/materialize-css/dist/js/materialize.min.js';
+import '../node_modules/@mdi/font/css/materialdesignicons.min.css';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
 import './styles.scss';
 import App from './App';
 
